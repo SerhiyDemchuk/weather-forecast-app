159b37a7ba269a1cd5c6b627ae1c399a55aac56e
SerhiyDemchuk <sergeylobster@gmail.com>
2020-11-25 23:27:15 +0200

    commit

--- src/redux/fetchApi/index.js
+++ src/redux/fetchApi/index.js
@@ -3,7 +3,7 @@ import { loadDataSuccess, loadDataFailure, loadDataRequest } from '../actions';
 
 // here we get a value from input to be placed for getting an info about city's weather
 export function fetchApi(value) {
-  return async (dispatch) => {
+  return (dispatch) => {
     dispatch(loadDataRequest());
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=f10b637ca08b184f47102d9f1de2c344`)
     // in a case of a correct value we get info about city's weather
